7771917ed44f075efd0b25429ace51d7965ad4fd
FilipSery <98544649+FilipSery@users.noreply.github.com>
2022-03-20 19:39:55 +0100

    pridani do vsc

---
+++ src/main/java/Nasobeni.java
@@ -0,0 +1,10 @@
+public class Nasobeni {
+    public int nasobeni (int a, int b){
+        int c = a*b;
+        return c;
+    }
+
+    public static void main (String[] args){
+        System.out.println("součin");
+    }
+}
---
+++ src/test/java/NaobeniTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class NaobeniTest {
+
+    public class NasobeniTest {
+        @Test
+        public void testNasobeniOk() {
+
+            Nasobeni nasobeni1 = new Nasobeni();
+            nasobeni1.nasobeni(5,6);
+            assertEquals(30,nasobeni1.nasobeni(5,6));
+
+
+        }
+        @Test
+        public void testNasobeniFail () {
+            Nasobeni nasobeni1 = new Nasobeni();
+            nasobeni1.nasobeni(5, 6);
+            assertEquals(29, nasobeni1.nasobeni(5, 6));
+        }
+    }
+}
