54c5ca62b99afe6c7e33e4b5739da41def58adcd
FilipSery <98544649+FilipSery@users.noreply.github.com>
2022-03-21 17:39:07 +0100

    Update NaobeniTest.java

--- src/test/java/NaobeniTest.java
+++ src/test/java/NaobeniTest.java
@@ -5,20 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NaobeniTest {
 
+//         @Test
+//         public void testNasobeniOk() {
+
+//             Nasobeni nasobeni1 = new Nasobeni();
+//             nasobeni1.nasobeni(5,6);
+//             assertEquals(30,nasobeni1.nasobeni(5,6));
+
+
+//         }
         @Test
-        public void testNasobeniOk() {
-
+        public void testNasobeniFail () {
             Nasobeni nasobeni1 = new Nasobeni();
-            nasobeni1.nasobeni(5,6);
-            assertEquals(30,nasobeni1.nasobeni(5,6));
-
-
+            nasobeni1.nasobeni(5, 6);
+            assertEquals(29, nasobeni1.nasobeni(5, 6));
         }
-//        @Test
-//        public void testNasobeniFail () {
-//            Nasobeni nasobeni1 = new Nasobeni();
-//            nasobeni1.nasobeni(5, 6);
-//            assertEquals(29, nasobeni1.nasobeni(5, 6));
-//        }
     }
 
